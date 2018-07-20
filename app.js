5290c588ab1246caa39821dd2121d292c8229d6f
ghildreth <gabrielantonhildreth@gmail.com>
2018-07-19 22:57:54 -0700

    add nodemon, images, and express route

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
+app.set('view engine', 'html');
+// app.use(express.static(path.join(__dirname, 'public')));
+app.engine('html', require('ejs').renderFile);
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.render('index'))
 
 app.listen(3000, () => console.log('Example appl listening on port 3000!'))
